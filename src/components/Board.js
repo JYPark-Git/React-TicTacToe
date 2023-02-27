30b5f15200bd3ec00513c4ac867f65c71c4383ae
JYPark-Git <jypark.git@gmail.com>
2023-02-27 23:04:12 +0900

    first commit

---
+++ src/components/Board.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Square from './Square';  /* export default */
+import './Board.css';
+
+export default class Board extends Component {
+
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    return (<div>
+      <div className='status'>Next Player : </div>
+      <div className='board-row'>
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        {this.renderSquare(3)}
+      </div>
+      <div className='board-row'>
+        {this.renderSquare(4)}
+        {this.renderSquare(5)}
+        {this.renderSquare(6)}
+      </div>
+      <div className='board-row'>
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
+        {this.renderSquare(9)}
+      </div>
+    </div>);
+  }
+}
